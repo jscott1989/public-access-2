1b6e385ef3263d810b6497daaa996ad3e3a3467e
Jonathan Scott <jonathan@jscott.me>
2014-07-01 09:04:32 +0100

    Start of level countdown

---
+++ Assets/Scripts/Countdown.cs
@@ -0,0 +1,17 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Countdown : MonoBehaviour {
+	public float countdownTimer = 30;
+
+	// Use this for initialization
+	void Start () {
+	
+	}
+	
+	// Update is called once per frame
+	void Update () {
+		countdownTimer -= Time.deltaTime;
+		guiText.text = countdownTimer.ToString ("0");
+	}
+}
