86338d51eacb76d59076bafafab619699618ad71
Jonathan Scott <jonathan@jscott.me>
2014-07-01 18:12:23 +0100

    Added static and sound to evening view (and renamed "Afternoon" to evening - my mistake!) - started on morning view

--- Assets/Scripts/Countdown.cs
+++ Assets/Scripts/Countdown.cs
@@ -1,16 +1,19 @@
 ﻿using UnityEngine;
 using System.Collections;
 
+/**
+ * Attach this script to a guiText object and it will manage a countdown from the provided number
+ * TODO: Set a callback for when the countdown completes
+ */
 public class Countdown : MonoBehaviour {
+
+	/**
+	 * The number that should be counted down from
+	 */
 	public float countdownTimer = 30;
 
-	// Use this for initialization
-	void Start () {
-	
-	}
-	
-	// Update is called once per frame
 	void Update () {
+		// Make the change and update the text
 		countdownTimer -= Time.deltaTime;
 		guiText.text = countdownTimer.ToString ("0");
 	}
