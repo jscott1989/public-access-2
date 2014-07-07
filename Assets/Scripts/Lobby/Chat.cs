89feed9e5ee288e29d4acd493086071cecd1ed1b
priyankasingh <singh.priyanka.7@gmail.com>
2014-07-07 19:28:26 +0100

    Chat script

---
+++ Assets/Scripts/Lobby/Chat.cs
@@ -0,0 +1,40 @@
+﻿using UnityEngine;
+
+[RequireComponent(typeof(UIInput))]
+
+public class Chat : MonoBehaviour
+{
+	public UITextList textList;
+
+	UIInput mInput;
+	
+	void Start ()
+	{
+		mInput = GetComponent<UIInput>();
+		
+
+	}
+
+	void AddMessage(string message)
+	{
+		textList.Add (message);
+
+	}
+
+	
+	void OnSubmit ()
+	{
+		if (textList != null)
+		{
+			// It's a good idea to strip out all symbols as we don't want user input to alter colors, add new lines, etc
+			string text = NGUITools.StripSymbols(mInput.text);
+			
+			if (!string.IsNullOrEmpty(text))
+			{
+				mInput.text = "";
+
+			}
+		}
+
+	}
+}
