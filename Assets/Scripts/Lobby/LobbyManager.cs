9163e768fe0a35aa7caa22100797f4d118941688
Jonathan Scott <jonathan@jscott.me>
2014-07-07 21:57:14 +0100

    Fixed it so all players get informed of other player's names - and players get incrementing numeric names

--- Assets/Scripts/Lobby/LobbyManager.cs
+++ Assets/Scripts/Lobby/LobbyManager.cs
@@ -5,6 +5,7 @@ using System.Collections;
 public class LobbyManager : MonoBehaviour {
 
 	public ChatTextList textList;
+	int playerNameIncrementor = 1;
 
 	GameObject[] existingPlayers = new GameObject[]{};
 
@@ -12,22 +13,33 @@ public class LobbyManager : MonoBehaviour {
 	void Start () {
 	
 	}
+
+	void AnnounceAllPlayerNames() {
+		foreach (GameObject p in existingPlayers) {
+			Player player = (Player)p.GetComponent (typeof(Player));
+			if (player.player_name == "") {
+				// This player is new, set a new name
+				player.SetPlayerName ("Player " + playerNameIncrementor.ToString ());
+				playerNameIncrementor += 1;
+				textList.Add (player.player_name + " has joined");
+			} else {
+				player.SetPlayerName (player.player_name);
+			}
+		}
+	}
 	
 	// Update is called once per frame
 	void Update () {
-		GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
-		foreach (GameObject p in players) {
-			if (!(existingPlayers.Contains (p))) {
-			
-				if (Network.isServer) {
-					Player player = (Player)p.GetComponent(typeof(Player));
-					player.SetPlayerName("test player");
-					textList.Add ("A Player has joined");
+		if (Network.isServer) {
+			GameObject[] players = GameObject.FindGameObjectsWithTag ("Player");
+			foreach (GameObject p in players) {
+				if (!(existingPlayers.Contains (p))) {
+					existingPlayers = players;
+					// There is at least one new player 
+					AnnounceAllPlayerNames();
+					return;
 				}
-
 			}
 		}
-
-		existingPlayers = players;
 	}
 }
