d556ee95432a0786c4ccf6a5efec1fe3540e0a4e
priyankasingh <singh.priyanka.7@gmail.com>
2014-07-07 18:18:10 +0100

    Network functionality

---
+++ Assets/Scripts/MainMenu/RefreshList.cs
@@ -0,0 +1,14 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RefreshList : MonoBehaviour {
+
+	public NetworkManager networkManager ;
+
+	void OnClick()
+	{
+			networkManager.RefreshHostList();
+		
+
+	}
+}
