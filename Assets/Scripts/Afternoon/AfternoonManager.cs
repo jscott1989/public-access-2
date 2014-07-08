eb6850cdf28d575f54fb0cffb748c2a811a84534
Jonathan Scott <jonathan@jscott.me>
2014-07-08 23:23:05 +0100

    added quick method for "purchasing" props across the network - so i can start on recording scene

--- Assets/Scripts/Afternoon/AfternoonManager.cs
+++ Assets/Scripts/Afternoon/AfternoonManager.cs
@@ -11,8 +11,18 @@ public class AfternoonManager : MonoBehaviour {
 	NetworkManager networkManager;
 
 	void Start () {
+		// First set up the references
 		networkManager = (NetworkManager) GameObject.FindGameObjectWithTag ("NetworkManager").GetComponent ("NetworkManager");
 		sceneRecorder = (SceneRecorder)GetComponent (typeof(SceneRecorder));
+
+		// Next - create the grid of available props
+		Player myPlayer = networkManager.GetMyPlayer ();
+
+		foreach (string prop_id in myPlayer.props) {
+			print (prop_id);
+		}
+
+
 
 		Action preparingCompleted =
 			() => StartRecording();
--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,9 +1,34 @@
 ﻿using UnityEngine;
 using System.Collections;
+using System.Collections.Generic;
 
 public class Player : MonoBehaviour {
 
 	public string player_name;
+
+	// TODO: Figure out a decent default budget - depends on the price of props
+	public int budget = 200;
+
+	// This is an array of strings which are identifiers for all of the props the player has purchased
+	public List<string> props = new List<string>();
+
+	void Start() {
+		// TODO: Get rid of this once the purchasing functionality is working
+		// We just set some default props so the recording can be tested
+		purchase (100, "bible");
+		purchase (100, "bear");
+	}
+
+	[RPC] public void purchase(int price, string prop_id) {
+		if (budget >= price) {
+			props.Add (prop_id);
+			budget -= price;
+			if (networkView.isMine) {
+				// If this is our player then tell the others about the purchase
+				networkView.RPC ("purchase", RPCMode.Others, price, prop_id);
+			}
+		}
+	}
 
 	[RPC] public void SetPlayerName(string player_tempname) {
 		player_name = player_tempname;
