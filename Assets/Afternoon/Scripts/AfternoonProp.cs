471f6706003ece8b195c3718ee9b4caeade452c3
Jonathan Scott <jonathan@jscott.me>
2014-07-13 15:37:24 +0100

    Draggable props

--- Assets/Afternoon/Scripts/AfternoonProp.cs
+++ Assets/Afternoon/Scripts/AfternoonProp.cs
@@ -4,11 +4,28 @@ using System.Collections;
 public class AfternoonProp : MonoBehaviour {
 	public PurchasedProp uPurchasedProp;
 	public dfTextureSprite uImage;
+	public GameObject mScreen;
+
+	GameObject mRecordingPropPrefab;
 	
-	MyProps mMyProps;
+	Props mProps;
 	
 	void Awake() {
-		mMyProps = (MyProps) FindObjectOfType(typeof(MyProps));
+		mProps = (Props) FindObjectOfType(typeof(Props));
 		uImage = (dfTextureSprite) GetComponentInChildren(typeof(dfTextureSprite));
+		mRecordingPropPrefab = (GameObject)Resources.Load ("Afternoon/Prefabs/RecordingProp");
+		mScreen = GameObject.FindGameObjectWithTag("Screen");
+	}
+
+	void OnMouseDown() {
+		gameObject.transform.parent = mScreen.transform;
+
+		GameObject g = (GameObject) Instantiate(mRecordingPropPrefab, Vector3.zero, Quaternion.identity);
+		g.transform.parent = mScreen.transform;
+		g.transform.position = gameObject.transform.position;
+
+		// TODO: Set this gameObject's parent back
+		mProps.Remove (uPurchasedProp);
+		Destroy (gameObject); // And don't destroy it
 	}
 }
---
+++ Assets/Afternoon/Scripts/MovableProp.cs
@@ -0,0 +1,50 @@
+﻿using UnityEngine;
+using System.Collections;
+
+/**
+ * A prop on the Afternoon scene which has been pulled from the prop box and is live
+ * This adds the ability for it to be moved, resized, and rotated, by the player
+ */
+public class MovableProp : MonoBehaviour {
+	bool mIsDragging = false;
+
+	dfPanel mScreenPanel;
+	dfTextureSprite mSprite;
+
+	// The drag offset
+	float mXOffset;
+	float mYOffset;
+
+	void Awake() {
+		mScreenPanel = (dfPanel)GameObject.FindGameObjectWithTag("Screen").GetComponent(typeof(dfPanel));
+		mSprite = (dfTextureSprite)GetComponent(typeof(dfTextureSprite));
+	}
+
+	void Update() {
+		if (mIsDragging) {
+			Vector2 aPosition = mScreenPanel.GetManager ().ScreenToGui(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+			aPosition = new Vector2(aPosition.x, 0 - aPosition.y);
+			// TODO: these numbers are hardcoded - copied from the unity gui - figure out how to get this programatically
+			Vector2 relativePosition = new Vector2(aPosition.x - 183, aPosition.y + 123);
+			print(mScreenPanel.Position);
+			mSprite.Position = new Vector2(relativePosition.x - mXOffset, relativePosition.y - mYOffset);
+
+			// Left = 183
+			// Top = 123
+			if (!Input.GetMouseButton(0)) {
+				mIsDragging = false;
+			}
+		}
+	}
+
+	void OnMouseDown() {
+		Vector2 aPosition = mScreenPanel.GetManager ().ScreenToGui(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+		aPosition = new Vector2(aPosition.x - 183, (0 - aPosition.y) + 123);
+		Vector2 bPosition = new Vector2(aPosition.x - mSprite.Position.x, aPosition.y - mSprite.Position.y);
+
+		mXOffset = bPosition.x;
+		mYOffset = bPosition.y;
+
+		mIsDragging = true;
+	}
+}
