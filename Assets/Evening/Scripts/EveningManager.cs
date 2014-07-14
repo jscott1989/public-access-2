185228a57a4d4c9f7358dd5e1ab369842663a300
Jonathan Scott <jonathan@jscott.me>
2014-07-14 17:38:50 +0100

    Channel changing

--- Assets/Evening/Scripts/EveningManager.cs
+++ Assets/Evening/Scripts/EveningManager.cs
@@ -7,16 +7,43 @@ public class EveningManager : SceneManager {
 	RecordingPlayer mRecordingPlayer;
 	GameObject mScreen;
 
+	int watchingPlayerNumber = 0;
+
 	void Awake() {
 		mNetworkManager = (NetworkManager) FindObjectOfType(typeof(NetworkManager));
 		mRecordingPlayer = (RecordingPlayer) FindObjectOfType(typeof(RecordingPlayer));
 		mScreen = GameObject.FindGameObjectWithTag("Screen");
 	}
 	void Start () {
-		mRecordingPlayer.Play(mNetworkManager.myPlayer, mScreen);
+		mRecordingPlayer.Play(mNetworkManager.players[watchingPlayerNumber], mScreen);
 	}
 
 	public void Jump() {
 		mRecordingPlayer.Jump (10);
+	}
+
+	public void ChannelUp() {
+		watchingPlayerNumber += 1;
+		if (watchingPlayerNumber >= mNetworkManager.players.Length) {
+			watchingPlayerNumber = 0;
+		}
+		mRecordingPlayer.Play(mNetworkManager.players[watchingPlayerNumber], mScreen);
+	}
+
+	public void ChannelDown() {
+		watchingPlayerNumber -= 1;
+		if (watchingPlayerNumber < 0) {
+			watchingPlayerNumber = mNetworkManager.players.Length - 1;
+		}
+		mRecordingPlayer.Play(mNetworkManager.players[watchingPlayerNumber], mScreen);
+	}
+
+	void Update() {
+		if (Input.GetKeyDown(KeyCode.UpArrow)) {
+			ChannelUp();
+		}
+		if (Input.GetKeyDown(KeyCode.DownArrow)) {
+			ChannelDown();
+		}
 	}
 }
