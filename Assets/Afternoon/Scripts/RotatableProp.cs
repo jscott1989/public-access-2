0a2470c702ff76c8c234d1b0d6c4f5ddb9d30624
Jonathan Scott <jonathan@jscott.me>
2014-07-23 21:27:06 +0100

    Can rotate (no icons, no recording, and some jerky positioning)

---
+++ Assets/Afternoon/Scripts/RotatableProp.cs
@@ -0,0 +1,34 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class RotatableProp : MonoBehaviour {
+	bool mIsDragging = false;
+
+	Vector2 mOriginalPosition;
+	float mOriginalRotation;
+
+	dfTextureSprite mSprite;
+
+	void Awake() {
+		mSprite = GetComponentInParent<dfTextureSprite>();
+	}
+
+	void OnMouseDown() {
+		mOriginalPosition = mSprite.GetManager ().ScreenToGui(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+		mOriginalRotation = mSprite.transform.eulerAngles.z;
+		mIsDragging = true;
+	}
+
+	void Update() {
+		if (mIsDragging) {
+			Vector2 position = mSprite.GetManager ().ScreenToGui(new Vector2(Input.mousePosition.x, Input.mousePosition.y));
+			float zRotation =  mOriginalRotation + (mOriginalPosition.x - position.x) + (mOriginalPosition.y - position.y);
+			print(zRotation);
+			mSprite.transform.eulerAngles = new Vector3(0,0, zRotation);
+
+			if (!Input.GetMouseButton(0)) {
+				mIsDragging = false;
+			}
+		}
+	}
+}
