d55629eeba74a2b03754441553833ca673c2d6bf
priyankasingh <singh.priyanka.7@gmail.com>
2014-07-24 19:55:05 +0100

    Changed audio

---
+++ Assets/Scripts/Playlist.cs
@@ -0,0 +1,44 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Playlist : MonoBehaviour {
+
+	public AudioClip[] uAudioClips;
+	private int mCurrentIndex = 0;
+
+	AudioSource mAudioSource;
+
+	void Awake(){
+
+		mAudioSource = GetComponent<AudioSource> ();
+
+	}
+
+	void Start(){
+
+		PlayNextClip ();
+
+	}
+
+	void PlayNextClip(){
+	
+		mCurrentIndex += 1;
+		if (mCurrentIndex >= uAudioClips.Length) {
+			mCurrentIndex = 0;		
+		}
+
+		mAudioSource.clip = uAudioClips [mCurrentIndex];
+
+		mAudioSource.Play ();
+	}
+
+	void Update(){
+
+
+		if (!mAudioSource.isPlaying) {
+		
+			PlayNextClip();
+		}
+	}
+
+}
